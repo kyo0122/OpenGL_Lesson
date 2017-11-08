f56d2a3f74fd3a10d7791cc9d2ac8bef1cbec851
kyosuke <hw14a117@oecu.jp>
2017-11-09 01:31:22 +0900

    ウィンドウ表示の実装
    解説は途中

--- OpenGL_Lesson/main.cpp
+++ OpenGL_Lesson/main.cpp
@@ -7,9 +7,65 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+#include <GLFW/glfw3.h>
+
+#pragma clang pop
+
+
+
+int main() {
+    
+    // glfwの初期化です。失敗するとTrueを返します。必ず実行してください。
+    if (!glfwInit()){
+        return -1;
+    }
+    
+    // OpenGLのバージョンなどの設定です。
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_DECORATED ,true);  // <-これをfalseにすると…
+    
+    
+    GLFWwindow* window;                     // glfwでは複数のウィンドウを利用することができます。
+    int width           =   640;            // 初期生成時のウィンドウの横幅です。
+    int height          =   480;            // 初期生成時のウィンドウの縦幅です。
+    const char* windowName    =   "ｳｨﾝﾄﾞｳﾀﾞﾖ-🌝 〜そして日本語も〜";  // ウィンドウに表示される名前です。
+    
+    // glfwCreateWindow:ウィンドウを生成します。左側のNULLをglfwGetPrimaryMonitor()にすると、フルスクリーンモードになります。
+    window = glfwCreateWindow(width, height, windowName, NULL, NULL);
+    
+    // ウィンドウ生成が失敗するとTrueを返します。
+    if (!window){
+        glfwTerminate();    // glfwの終了時に呼び出す関数です。
+        return -1;
+    }
+    
+    // ウィンドウのコンテキストをカレントにします。(これから行う処理は指定したwindowに対するものですよ…的なことですよ、多分。)
+    glfwMakeContextCurrent(window);
+    
+    
+    // glfwWindowShouldClose:ウィンドウが閉じられた時にTrueを返します。
+    // glfwGetKey: 指定windowでのキー入力を取得します。状態確認はGLFW_PRESSなどで確認します。
+    while (!glfwWindowShouldClose(window)&&glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(.3, .3, .3, 1);
+        
+        
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwTerminate();
+    
+    
     return 0;
 }
