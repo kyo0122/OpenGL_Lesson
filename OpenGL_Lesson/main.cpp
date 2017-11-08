6b35f106d4acb2b6574d32f1d896d3d893a13ec4
kyosuke <hw14a117@oecu.jp>
2017-11-08 17:14:56 +0900

    Initial Commit

---
+++ OpenGL_Lesson/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL_Lesson
+//
+//  Created by wta1601 on 2017/11/08.
+//  Copyright © 2017年 kyo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
