9c8d1338b86a5a67ca9b520cb4e558da70f87061
kyosuke <kyosuke@muratakyousukenoMacBook-Pro.local>
2017-11-10 04:42:39 +0900

    カメラクラスとシェーダーの追加
    カメラの移動に対応してオブジェクトが動くシェーダーの追加

---
+++ OpenGL_Lesson/Camera.cpp
@@ -0,0 +1,9 @@
+//
+//  Camera.cpp
+//  OpenGL_Lesson
+//
+//  Created by kyosuke on 2017/11/10.
+//  Copyright © 2017年 kyo. All rights reserved.
+//
+
+#include "Camera.hpp"
---
+++ OpenGL_Lesson/Camera.hpp
@@ -0,0 +1,14 @@
+//
+//  Camera.hpp
+//  OpenGL_Lesson
+//
+//  Created by kyosuke on 2017/11/10.
+//  Copyright © 2017年 kyo. All rights reserved.
+//
+
+#ifndef Camera_hpp
+#define Camera_hpp
+
+#include <stdio.h>
+
+#endif /* Camera_hpp */
