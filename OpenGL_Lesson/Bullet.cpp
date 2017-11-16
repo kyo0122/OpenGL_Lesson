a5ca6361ef6d1e88b9d480e1cf175ba9dc8556ab
kyosuke <kyosuke@muratakyousukenoMacBook-Pro.local>
2017-11-16 22:43:42 +0900

    弾クラスを追加

---
+++ OpenGL_Lesson/Bullet.cpp
@@ -0,0 +1,9 @@
+//
+//  Bullet.cpp
+//  OpenGL_Lesson
+//
+//  Created by kyosuke on 2017/11/16.
+//  Copyright © 2017年 kyo. All rights reserved.
+//
+
+#include "Bullet.hpp"
---
+++ OpenGL_Lesson/Bullet.hpp
@@ -0,0 +1,14 @@
+//
+//  Bullet.hpp
+//  OpenGL_Lesson
+//
+//  Created by kyosuke on 2017/11/16.
+//  Copyright © 2017年 kyo. All rights reserved.
+//
+
+#ifndef Bullet_hpp
+#define Bullet_hpp
+
+#include <stdio.h>
+
+#endif /* Bullet_hpp */
