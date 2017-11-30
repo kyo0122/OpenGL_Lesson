6efd5a50779a9344493c567be7fbac111905213e
kyosuke <kyosuke@muratakyousukenoMacBook-Pro.local>
2017-12-01 05:31:59 +0900

    日本語表示の実装

--- OpenGL_Lesson/main.cpp
+++ OpenGL_Lesson/main.cpp
@@ -27,7 +27,7 @@ int main() {
     
     initWindowHints();
     
-    GLFWwindow* window = glfwCreateWindow(640, 480, "☀️☁️", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "🎫→📱", NULL, NULL);
     if (!window){
         glfwTerminate();
         return -1;
@@ -49,11 +49,31 @@ int main() {
     skybox.SetTexture("winter-2383930_1280.png");
     
     Camera cam(vec3(0, 0, 0), window);
+    cam.controller(window);
     
     // ImGuiの初期化処理です
-    // 二つ目の引数はコールバックを行うかどうかです。
-    // 詳しくはimgui_impl_glfw_gl3.cppのImGui_ImplGlfwGL3_Initを確認してください。
     ImGui_ImplGlfwGL3_Init(window, false);
+    
+    // ImGuiのフォント設定です。
+    // 日本語を使う場合は必須の処理です。
+    ImGuiIO& io = ImGui::GetIO();
+    io.Fonts->AddFontFromFileTTF("sawarabi-gothic-medium.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+    
+    // ImGui独自のカーソル表示です
+//    io.MouseDrawCursor = true;
+    
+    // ImGuiのウィンドウ設定です
+    ImGuiWindowFlags flags = 0;
+    flags |= ImGuiWindowFlags_NoTitleBar;
+    flags |= ImGuiWindowFlags_NoResize;
+//    flags |= ImGuiWindowFlags_NoMove;
+    
+    // ImGuiのスタイル設定です
+    ImGuiStyle &style = ImGui::GetStyle();
+    style.Colors[ImGuiCol_Text] = ImVec4(0, 0, 0, 1);
+    style.Colors[ImGuiCol_WindowBg] = ImVec4(0, 0, 0, 0);
+    
+    bool toggle = false;
     
     while (!glfwWindowShouldClose(window)&&glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS)
     {
@@ -63,20 +83,33 @@ int main() {
         // フレーム毎にImGuiの入力等を準備する関数です
         ImGui_ImplGlfwGL3_NewFrame();
         
+        if(glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS){
+            toggle = true;
+        }
+        if(glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS){
+            toggle = false;
+        }
+        
+        if(toggle){
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }else{
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+            cam.controller(window);
+        }
+        
         // ブロック内がGUIに関する処理です。
         // OpenGLの説明範囲外なので、詳しい処理の説明は行いません。
         // imgui.cppの最上部に説明が書かれているので、
         // そちらを参照してください。
         {
-            ImGui::Begin("TestWindow");
-            static float f = 0.0f;
-            ImGui::Text("Hello, world!");
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
+            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiSetCond_FirstUseEver);
+            ImGui::Begin("テストウィンドウ", NULL, flags);
+            ImGui::Text("こんにちworld!");
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
             ImGui::End();
         }
         
-        cam.controller(window);
+        
         
         skybox.position = cam.GetPosition();
         skybox.Rendering(cam);
@@ -99,7 +132,7 @@ void initWindowHints()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_DECORATED ,true);  // <-これをfalseにすると…
+    glfwWindowHint(GLFW_DECORATED ,true);
 }
 
 
