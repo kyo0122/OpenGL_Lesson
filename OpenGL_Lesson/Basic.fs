3d714906a232c58ff60c9770883457a3240c2b17
kyosuke <>
2017-12-20 00:19:05 +0900

    シェーダーの変更
    アンビエント等のパラメーターを追加

--- OpenGL_Lesson/Basic.fs
+++ OpenGL_Lesson/Basic.fs
@@ -15,31 +15,40 @@ void main() {
     vec3 LightColor = vec3(1,1,1);
     float LightPower = 10.0f;
     
+    // 鏡面反射光、拡散反射光などの設定です。
+    vec3 MaterialDiffuseColor = texture(textureSampler, UV).rgb;
+    vec3 MaterialAmbientColor = vec3(0.1,0.1,0.1) * MaterialDiffuseColor;
+    vec3 MaterialSpecularColor = vec3(0.3,0.3,0.3);
+    
     float distance_ = length( LightPosition_w - Position_w );
     // カメラ空間で、計算されたフラグメントの法線
     vec3 n = normalize( Normal_c );
     // 光の方向(フラグメントから光の方向)
     vec3 l = normalize( LightDirection_c );
-    
+
     // 法線と光の方向の角度のコサイン
     // clamped above 0
     //  - 光が三角形に水平の場合 -> 1
     //  - 光が三角形に垂直の場合 -> 0
     //  - 光が三角形の裏側の場合 -> 0
     float cosTheta = clamp( dot( n,l ), 0,1);
-    
+
     // アイ(目)ベクトル(カメラのほうへ向かう)
     vec3 E = normalize(EyeDirection_c);
     // 三角形が光を反射する方向
     vec3 R = reflect(-l,n);
-    
+
     // アイベクトルと反射ベクトルの間の角度のコサイン
     // 0以上に固定します。
     //  - 反射する方向を向いている -> 1
     //  - 他の方向を見ている -> < 1
     float cosAlpha = clamp( dot( E,R ), 0,1 );
-    
-    // texture(textureSampler, UV).bgr
-    color = texture(textureSampler, UV).bgr * LightColor * LightPower * cosTheta / (distance_*distance_) + LightColor * LightPower * pow(cosAlpha,5) / (distance_*distance_);
+
+    // Ambient : ざっとした間接光
+    color = MaterialAmbientColor +
+    // Diffuse : オブジェクトの色
+    MaterialDiffuseColor * LightColor * LightPower * cosTheta / (distance_*distance_) +
+    // Specular : 反射光
+    MaterialSpecularColor * LightColor * LightPower * pow(cosAlpha,5) / (distance_*distance_);
 }
 
--- OpenGL_Lesson/main.cpp
+++ OpenGL_Lesson/main.cpp
@@ -42,27 +42,9 @@ int main() {
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glEnable(GL_DEPTH_TEST);
     
-    // 削除するモデルを配列として保持しておきます
-    vector<ModelObject> models;
-    
     ModelObject monkey1("monkey.obj", "Basic");
-    monkey1.position = vec3(2, 0, 0);
-    models.push_back(monkey1);
-    
-    ModelObject monkey2("monkey.obj", "Basic");
-    monkey2.position = vec3(-2, 0, 0);
-    models.push_back(monkey2);
-    
-    ModelObject monkey3("monkey.obj", "Basic");
-    monkey3.position = vec3(0, 2, 0);
-    models.push_back(monkey3);
-    
-    ModelObject monkey4("monkey.obj", "Basic");
-    monkey4.position = vec3(0, -2, 0);
-    models.push_back(monkey4);
-    
-    // 上の如く、弾も然り
-    vector<Bullet> bullets;
+    monkey1.SetTexture("UVCheckerMap01-1024.png");
+    monkey1.position = vec3(0, 0, -4);
     
     ModelObject skybox("skybox.obj", "Skybox");
     skybox.SetTexture("winter-2383930_1280.png");
@@ -75,40 +57,7 @@ int main() {
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         
-        // 弾を発射
-        if(glfwGetKey(window, GLFW_KEY_X)==GLFW_PRESS){
-            Bullet b(cam.GetDirection());
-            b.position = cam.GetPosition();
-            bullets.push_back(b);
-        }
-        
-        // 全ての弾と猿(modelsの要素)との距離を計算。
-        // 一定値以下の(接近している)状態で、描画無視フラグをオン
-        for(auto &bullet : bullets){
-            for(auto &model : models){
-                auto d = distance(bullet.position, model.position);
-                if(d < 0.5){
-                    model.isDead = true;
-                }
-            }
-        }
-        
-        /*
-         *for(int i = 0; i < bullets.size; i++){
-         *  bullet[i].Update();
-         *  bullet[i].Rendering(cam);
-         *}                             と同じ意味です。
-         */
-        for(auto &bullet : bullets){
-            bullet.Update();
-            bullet.Rendering(cam);
-        }
-        
-        for(auto &model : models){
-            if(!model.isDead){
-                model.Rendering(cam);
-            }
-        }
+        monkey1.Rendering(cam);
         
         cam.controller(window);
         
